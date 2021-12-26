c977c6e42a8a35e952e0ad2a00734d30a88dfebd
Hamza Aman <thehamzaaman@gmail.com>
2021-12-26 19:57:21 +0500

    subscription integrated and flow completed

--- carmapclient/components/CarMap.tsx
+++ carmapclient/components/CarMap.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useRef} from 'react';
-import {StyleSheet, View} from 'react-native';
-
-import MapView from 'react-native-maps';
+import React, { useState, useEffect, useRef } from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import { gql, useSubscription } from '@apollo/client';
+import MapView, { Marker } from 'react-native-maps';
 
 import SlidingUpPanel from 'rn-sliding-up-panel';
 import CloseButton from './CloseButton';
@@ -12,17 +12,72 @@ type Car = {
   distanceToDestination: number;
 };
 
+const SUBSCRIPTION = gql`
+subscription {
+  car {
+    id
+    location {
+      latitude
+      longitude
+    }
+    distanceToDestination
+  }
+}
+`;
+
 const CarMap = () => {
   // At the top of this component you should subscribe to updates from the server.
   // See https://www.apollographql.com/docs/react/data/subscriptions/#executing-a-subscription
-
   const panelEl = useRef<SlidingUpPanel>(null);
+  const { data = [] } = useSubscription(SUBSCRIPTION)
+  const [coord, setCoord] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.1022,
+    longitudeDelta: 0.0521,
+  })
+  const [selectedCar, setSelectedCar] = useState<number>(0)
 
-  useEffect(() => panelEl.current?.show());
-
+  useEffect(() => {
+    if (data?.car?.[0]) {
+      const { latitude, longitude } = data?.car?.[4].location
+      setCoord({
+        ...coord,
+        latitude,
+        longitude
+      })
+    }
+  }, [data])
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825, // can use react-native-get-location to get current coordinates of the client. But it's not the requirement of the
+          longitude: -122.4324,
+          latitudeDelta: 0.1022,
+          longitudeDelta: 0.0521,
+        }}
+        region={coord}
+      >
+        {data?.car?.map((val: any , index: number) => {
+          const { latitude, longitude } = val.location
+          return (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude,
+                longitude
+              }}
+              image={require('./Car.png')}
+              onPress={() => {
+                setSelectedCar(index)
+                panelEl.current?.show()
+              }}
+            />
+          )
+        })}
+      </MapView>
       {/* For documentation on the MapView see https://github.com/react-native-maps/react-native-maps */}
       <SlidingUpPanel
         ref={panelEl}
@@ -31,10 +86,11 @@ const CarMap = () => {
         allowDragging={false}>
         <View style={styles.panel}>
           <CloseButton
-            onPress={() => {
-              panelEl.current?.hide();
-            }}
+            onPress={() => panelEl.current?.hide()}
           />
+          <Text style={styles.txtID}>Car #{data?.car?.[selectedCar].id}</Text>
+          <Text>Location: {`${data?.car?.[selectedCar].location?.latitude?.toFixed(5) }, ${data?.car?.[selectedCar].location?.longitude?.toFixed(5)}`}</Text>
+          <Text>Distance to Destination: {data?.car?.[selectedCar].distanceToDestination}</Text>
         </View>
       </SlidingUpPanel>
     </View>
@@ -59,6 +115,12 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingTop: 20,
   },
+  txtID: {
+    fontWeight: 'bold',
+    marginBottom: 12
+  }
 });
 
 export default CarMap;
+
+
